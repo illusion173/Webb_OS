5420f8901ed6109f30ae7cea751aebcba9ead71c
Jeremiah Webb <illusion173@hotmail.com>
2022-12-30 16:37:02 -0500

    added boot image functionality

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -1,14 +1,22 @@
 #![no_std] // no link to rust std
 #![no_main] // disable rust level entry pnt
-
+mod vga_buffer;
 use core::panic::PanicInfo;
-
-#[no_mangle] // don't mangle the name of this function
-pub extern "C" fn _start() -> ! {
-    loop {}
-}
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+static HELLO: &[u8] = b"Hello World!";
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
